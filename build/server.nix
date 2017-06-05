9544e3c6c08c12b1f58835b9923d4e6e4329a107
Michael Peyton Jones <michaelpj@gmail.com>
2017-06-05 21:04:40 +0100

    Redirection better

--- build/server.nix
+++ build/server.nix
@@ -6,8 +6,12 @@
     {
       services.nginx = {
         enable = true;
-        
+
         virtualHosts."michaelpj.com" = { 
+          extraConfig = "return 301 $scheme://www.michaelpj.com$request_uri;";
+        };
+        
+        virtualHosts."www.michaelpj.com" = { 
           root = pkgs.callPackage ./default.nix {};
           extraConfig = "rewrite ^/$ /blog redirect;";
         };
